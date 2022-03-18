22271a37b5aee116f34f6d7497ed4a1088cdf98a
Denis <laptev.denis.18@gmail.com>
2022-03-18 13:05:44 +0200

    work with GitHub

--- HelloWorlds.java
+++ HelloWorlds.java
@@ -2,5 +2,6 @@ public class HelloWorlds{
     public static void main(String[] args) {
         System.out.printf("Hello Worlds!");
 		System.out.printf("Hello!");
+		System.out.printf("Updated code");
     }
 }
